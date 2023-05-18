2235352624f78afb4378b2c4001fe66fd6a43ce2
Stephane Mensah <1687551+waptik@users.noreply.github.com>
2023-05-18 17:19:20 +0000

    Update preact version

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export { default as puppeteer } from "https://deno.land/x/puppeteer@9.0.0/mod.ts";
 export { Application, Router } from "https://deno.land/x/oak@v7.3.0/mod.ts";
-export * from "https://esm.sh/preact@10.5.12";
+export * from "https://esm.sh/preact@10.13.1";
 export { renderToString } from "https://esm.sh/*preact-render-to-string@5.2.6";
